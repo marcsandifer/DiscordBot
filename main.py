c5e162917d6069b1042a51e2ed323d8f573facb4
Marc Sandifer <marc@sandifer.me>
2023-01-08 12:24:35 +0100

    Added discord connect & message functionality

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+import random
+import discord
+import os
+from megahal import *
+from dotenv import load_dotenv
 
 
-# Press the green button in the gutter to run the script.
+def connect():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
+
+    client = discord.Client(intents=discord.Intents.all())
+
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        reply = await chat(str(message))
+        await message.channel.send(reply)
+
+    client.run(TOKEN)
+
+
+def count_lines():
+    with open('pegida_korpus.txt', encoding='UTF-8', errors='ignore') as trainingfile:
+        for count, line in enumerate(trainingfile):
+            pass
+        print('Number of lines in your file:', count + 1)
+
+
+def learn():
+    megahal = MegaHAL()
+    with open('pegida_korpus_small.txt', encoding='UTF-8', errors='ignore') as trainingfile:
+        current_line = 0
+        while True:
+            current_line += 1
+            line_content = trainingfile.readline()
+            print(current_line, line_content)
+            megahal.learn(line_content)
+            megahal.close()
+            megahal = MegaHAL()
+            if not line_content:
+                break
+        trainingfile.close()
+
+
+async def chat(message):
+    megahal = MegaHAL()
+    while True:
+        reply = megahal.get_reply(message)
+        print(reply)
+        return reply
+
+
+async def train():
+    megahal = MegaHAL()
+    megahal.train('')  # make sure your file doesn't include "'", as that breaks it
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    connect()
+    # count_lines()
+    # chat()
+    # learn()
+    # train()
